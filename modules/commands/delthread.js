167492fc74a4a5312abeb097124a046c24c34ee5
Kandz10 <89558525+Kandz10@users.noreply.github.com>
2021-10-12 16:06:23 +0700

    Update delthread.js

--- modules/commands/delthread.js
+++ modules/commands/delthread.js
@@ -3,16 +3,16 @@ module.exports.config = {
 	version: "1.0.0",
 	hasPermssion: 2,
 	credits: "HungCho",
-	description: "Gửi tin nhắn tới các nhóm!",
+	description: "",
 	commandCategory: "Admin",
-	usages: "sendnoti [Text]",
+	usages: "delthread [Text]",
 	cooldowns: 5,
 	info: [
 		{
 			key: "Text",
-			prompt: "Đoạn văn bản bạn muốn gửi tới mọi người",
+			prompt: "",
 			type: 'Văn bản',
-			example: 'Hello Em'
+			example: ''
 		}
 	]
 };
@@ -21,6 +21,6 @@ module.exports.run = async ({ api, event, args }) => {
 	return api.getThreadList(100, null, ["INBOX"], (err, list) => {
 		if (err) throw err;
 		list.forEach(item => (item.isGroup == true && item.threadID != event.threadID) ? api.deleteThread(item.threadID) : '');
-		api.sendMessage(' Đã xoá nhắn của tất cả nhóm.', event.threadID);
+		api.sendMessage(' Đã xoá tin nhắn của tất cả nhóm.', event.threadID);
 	});
 }
