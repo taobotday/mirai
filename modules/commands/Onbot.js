43cccda832db9a783287f7e7abc803537067f2a9
Minh Khiêm <87948587+taobotday@users.noreply.github.com>
2021-10-17 00:09:59 +0700

    Update Onbot.js

--- modules/commands/Onbot.js
+++ modules/commands/Onbot.js
@@ -1,10 +1,8 @@
 module.exports.config = {
-	name: "onbot",
+	name: "on",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "HTHB",
 	description: "Bật Bot.",
-	commandCategory: "AdminBot",
 	cooldowns: 0
         };
 module.exports.run = ({event, api}) =>api.sendMessage("onbot",event.threadID, () =>process.enter(1))
