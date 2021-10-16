845088a95e9be924885d18a83b166f64c1d23655
Minh Khiêm <87948587+taobotday@users.noreply.github.com>
2021-10-17 00:12:00 +0700

    Update off.js

--- modules/commands/off.js
+++ modules/commands/off.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "off",
 	version: "1.0.0",
 	hasPermssion: 2,
-	description: "tắt Bot",
+	description: "tắt bot",
 	cooldowns: 0
         };
 module.exports.run = ({event, api}) =>api.sendMessage("Bye Bitch ✅",event.threadID, () =>process.exit(0))
